0ead61a10ecf7f350b860a9d57286f936049fa1f
grigoriy-golovin <watson.180491@gmail.com>
2020-08-14 11:13:09 +0300

    добавил информацию о проектах

--- source/js/template.js
+++ source/js/template.js
@@ -1,12 +1,13 @@
 "use strict";
 
-var cardData = [
+const cardData = [
   {
     imgSrc: "img/sis-cities.png",
     imgAlt: "sis-cities",
     title: "Sis cities",
     link: "https://grigoriy-golovin.github.io/six-cities",
-    description: `SPA на React. Современный сервис
+    linkCode: "https://github.com/grigoriy-golovin/six-cities",
+    description: `Single Page Application выполненное на базе React. Современный сервис
      для путешественников, которые не хотят переплачивать за аренду жилья.
      В приложении можно получить актуальный
       список предложений по аренде в одном из шести популярных городов.
@@ -22,37 +23,51 @@ var cardData = [
     imgAlt: "pixiel-hanter",
     title: "Pixiel hanter",
     link: "https://grigoriy-golovin.github.io/pixel-hunter",
-    description: "",
-    techno: "",
+    linkCode: "https://github.com/grigoriy-golovin/pixel-hunter",
+
+    description: `Сайт - игра, в которой надо угадывать что тебе сейчас
+    показывают: фотография или рисунок?
+    Игра написана javaScript версии ECMAScript 2015+
+     без использования framework по паттерну MVP`,
+    techno: "ECMAScript 2015+, OOP, Promise, Fetch API",
   },
   {
     imgSrc: "img/keksobooking.png",
     imgAlt: "Keksbooking",
     title: "Keksbooking",
     link: "https://grigoriy-golovin.github.io/Keksbooking",
+    linkCode: "https://github.com/grigoriy-golovin/booking",
     description: `Сервис размещения объявлений об аренде
      недвижимости в Токио. Пользователи делятся информацией
      о своём объекте недвижимости, размещают фотографии, указывают
      адрес, перемещая метку по карте города. Кроме этого, можно
       посмотреть на объявления,
      размещённые другими пользователями.`,
-    techno: "DOM API, XMLHttpRequest, ",
+    techno: "DOM API, XMLHttpRequest, FormData",
   },
   {
     imgSrc: "img/culture-news.png",
     imgAlt: "culture-news",
     title: "Culture news",
     link: "https://grigoriy-golovin.github.io/culture-news",
-    description: "",
-    techno: "",
+    linkCode: "https://github.com/grigoriy-golovin/culture-news",
+    description: `Вёрстка сайта с новостями культуры.
+    Реализованна адаптивная и резиновая вёрстка с помощью Grid layout.`,
+    techno: "Less, Gulp, Flex, Picture, БЭМ, Node.js",
   },
   {
     imgSrc: "img/pink.png",
     imgAlt: "pink",
     title: "Pink",
     link: "https://grigoriy-golovin.github.io/pink",
-    description: "",
-    techno: "",
+    linkCode: "https://github.com/grigoriy-golovin/pink",
+    description: `Вёрстка сайта о приложении Pink.
+     Это приложение позволит раскрасить серые будни в яркие
+     краски буквально в один клик.
+     Вёрстка выполнена адоптивно с фиксированной сеткой
+     на три размера экрана на препроцессоре Less, сборка и оптимизация
+     выполнены с помощью Gulp.`,
+    techno: "Less, Gulp, Flex, Picture, SVG-спрайт, WebP, БЭМ, Node.js",
   },
   {
     imgSrc: "img/light.png",
@@ -60,18 +75,21 @@ var cardData = [
     title: "Light",
     link: "https://grigoriy-golovin.github.io/light",
     linkCode: "https://github.com/grigoriy-golovin/trialWork",
-    description: "",
-    techno: "",
+    description: `Простая вёрстка главной страницы сайта компании
+     по продаже светового оборудования.`,
+    techno: "HTML, CSS, Flex",
   },
 ];
 
-var cardInsert = document.querySelector(".portfolio__list");
-var templateCard = document
+const cardInsert = document.querySelector(".portfolio__list");
+const templateCard = document
   .querySelector("template")
   .content.querySelector(".card");
 
-var createSlid = function (data, template) {
-  var slid = template.cloneNode(true);
+const bgBlack =document.querySelector(`.card__bg-black`);
+
+const createSlid = function (data, template) {
+  const slid = template.cloneNode(true);
   slid.querySelector("img").src = data.imgSrc;
   slid.querySelector("img").alt = data.imgAlt;
   slid.querySelector(".card__title").textContent = data.title;
@@ -82,10 +100,12 @@ var createSlid = function (data, template) {
 
   slid.querySelector("img").onclick = () => {
     slid.classList.add(`card--active`);
+    bgBlack.classList.add(`card__bg-black--active`)
   };
 
   slid.querySelector(".card__but-close").onclick = () => {
     slid.classList.remove(`card--active`);
+    bgBlack.classList.remove(`card__bg-black--active`)
   };
 
   return slid;
@@ -94,3 +114,8 @@ var createSlid = function (data, template) {
 cardData.forEach((element) => {
   cardInsert.append(createSlid(element, templateCard));
 });
+
+bgBlack.onclick = () => {
+  document.querySelector(`.card--active`).classList.remove(`card--active`);
+  bgBlack.classList.remove(`card__bg-black--active`)
+}
